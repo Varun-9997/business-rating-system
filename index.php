8d0e1896fd078bafe92f2e167bab0f8de9a67fc5
Varun-9997 <dangrevarun@gmail.com>
2026-04-24 16:52:54 +0530

    project completed

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Business Listing & Rating System</title>
+<title>Business Listing & Rating System /title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
