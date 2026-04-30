b6123556809b21ca61618e0ffa3b33e4d765314f
Varun-9997 <dangrevarun@gmail.com>
2026-04-30 12:35:30 +0530

    cd

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php include "conn.php"; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Business Listing & Rating System /title>
+<title>Business Listing & Rating System </title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
